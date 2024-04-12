96f1e4428f29ea587e2ea5ec0e7d5b5913b0bc8f
Deep <deepjee2023@gmail.com>
2024-04-12 19:27:51 +0530

    #changes

---
+++ Login.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+
+# url = "https://docs.google.com/spreadsheets/d/11rAxWN1wftay-bIpcMB3NsjgLkgg4Sw8LFAt49cyC_k/edit?usp=sharing"
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+data = conn.read(worksheet="Sheet2",usecols=list(range(4)),  ttl=5)
+data = data.dropna(how="all")
+
+with st.form(key='personal_form'):
+    person_name=st.text_input(label='Name*')
+    age=st.number_input(label='Age*',step=1,value=None)
+    phone_number=st.number_input(label='Phone No.',step=1,value=None)
+    aadhar=st.number_input(label='Aadhar No.',step=1,value=None)
+    st.markdown("**required*")
+    submit_button=st.form_submit_button(label='Submit the form')
+
+    if submit_button:
+        if not person_name or not age or not aadhar:
+            st.warning("Ensure all mandatory fields are filled.")
+            st.stop()
+        elif data["Name"].str.contains(person_name).any():
+            st.warning("Another person with same name already exists.")
+            st.stop()
+        else:
+            person_data=pd.DataFrame(
+                [
+                    {
+                        "Name": person_name,
+                        "Age": age,
+                        "Phone": phone_number,
+                        "Aadhar": aadhar,
+                    }
+                ]
+            )
+
+            updated_df=pd.concat([data,person_data], ignore_index=True)
+
+            conn.update(worksheet="Sheet2",data=updated_df)
+
+            st.success("All details are successfully submited!")
+
+
+
---
+++ pages/calculator.py
@@ -0,0 +1,284 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Person Carbon Calculator")
+                   
+st.title(f"Welcome,")
+st.title("Personal Carbon Calculator App")
+
+region=st.selectbox("Select",['None','Urban','Rural'])
+
+if region=='Urban':
+    val = st.number_input('Electricity(KWh)', value = 0)
+    electricity=st.slider('',0,100,value=val,key='electricity')
+    gas_connection=st.selectbox("Gas Connection",['None','Gas Cylinder','Gas Pipeline'])
+    if gas_connection=='Gas Cylinder':
+        gas_cylinder=st.number_input('No. of gas cylinders',step=1,value=None)
+    elif gas_connection=='Gas Pipeline':
+        gas_pipeline=st.number_input('Gas Usage(cubic metre)',step=1,value=None)
+
+    transport=st.multiselect("Transport",['Air Travel','Train','Bus','Private Vehicle'])
+    if "Air Travel" in transport:
+        air_travel=st.selectbox("Mode of air travel",['None','Domestic','International'])
+        if air_travel=='Domestic':
+            domestic=st.number_input("No. of domestic flights",step=1,value=None)
+        if air_travel=='International':
+            domestic=st.number_input("No. of international flights",step=1,value=None)
+    if "Train" in transport:
+        train_travel=st.selectbox("Time traveled per month in train",['Less than 12hrs','between 12 and 24 hrs','More than 24 hrs'])
+        
+    if "Bus" in transport:
+        bus_travel=st.number_input('Distance traveled by Bus(Km)',step=1,value=None)
+    if "Private Vehicle" in transport:  
+        private_travel=st.selectbox("Fuel Type",['Petrol','Diseal'])
+        mileage=st.number_input('Mileage',step=1,value=None)
+        age=st.number_input('How Old is your car',step=1,value=None)
+        total_distance=st.number_input('Total Distance Driven',step=1,value=None)
+
+    food_type=st.selectbox("Veg/Non-Veg",['Veg','Non-Veg'])
+    val1 = st.number_input('Waste Generated(Kg)', value = 0)
+    waste=st.slider('',0,100,value=val1,key='waste')
+    mealperday=st.number_input("No of Meals per Day",step=1,value=None)
+    renewable=st.selectbox("Any type of renewable energy generated",["No","yes"])
+    if renewable=="yes":
+        amount_renewable=st.number_input("Amount of Energy Renewed(KWh)",step=1,value=None)
+
+
+    
+
+elif region=='Rural':
+    val = st.number_input('Electricity(KWh)', value = 0)
+    electricity=st.slider('',0,100,value=val,key='electricity')
+    gas_connection=st.selectbox("Gas Connection",['None','Gas Cylinder','Gas Pipeline'])
+    if gas_connection=='Gas Cylinder':
+        gas_cylinder=st.number_input('No. of gas cylinders',step=1,value=None)
+    elif gas_connection=='Gas Pipeline':
+        gas_pipeline=st.number_input('Gas Usage(cubic metre)',step=1,value=None)
+
+    transport=st.multiselect("Transport",['Air Travel','Train','Bus','Private Vehicle'])
+    if "Air Travel" in transport:
+        air_travel=st.selectbox("Mode of air travel",['None','Domestic','International'])
+        if air_travel=='Domestic':
+            domestic=st.number_input("No. of domestic flights",step=1,value=None)
+        if air_travel=='International':
+            domestic=st.number_input("No. of international flights",step=1,value=None)
+    if "Train" in transport:
+        train_travel=st.selectbox("Time traveled per month in train",['Less than 12hrs','between 12 and 24 hrs','More than 24 hrs'])
+        
+    if "Bus" in transport:
+        bus_travel=st.number_input('Distance traveled by Bus(Km)',step=1,value=None)
+    if "Private Vehicle" in transport:  
+        private_travel=st.selectbox("Fuel Type",['Petrol','Diseal'])
+        mileage=st.number_input('Mileage',step=1,value=None)
+        age=st.number_input('How Old is your car',step=1,value=None)
+        total_distance=st.number_input('Total Distance Driven',step=1,value=None)
+
+    food_type=st.selectbox("Veg/Non-Veg",['Veg','Non-Veg'])
+    val1 = st.number_input('Waste Generated(Kg)', value = 0)
+    waste=st.slider('',0,100,value=val1,key='waste')
+    mealperday=st.number_input("No of Meals per Day",step=1,value=None)
+    renewable=st.selectbox("Any type of renewable energy generated",["No","yes"])
+    if renewable=="yes":
+        amount_renewable=st.number_input("Amount of Energy Renewed(KWh)",step=1,value=None)
+
+submit_button=st.button("Calculate the CO₂ Emission")
+
+if submit_button:
+    st.header("Results")
+    
+
+# def get_query_params():
+#     return st.query_params
+
+
+# EMISSION_FACTORS = {
+        
+#     "India":{
+#         "Transportation": 0.225, #kgCO2/km(Average)
+#         "Electricity": 1.0, #kgCO2/Kwh (Average)
+#         "Diet": 1.325, #kgCO2/meal
+#         "Waste": 0.4 #kgCO2/kg
+#     },
+#     "SriLanka":{
+#          "Transportation": 0.14, #kgCO2/km
+#         "Electricity": 0.82, #kgCO2/Kwh
+#         "Diet": 1.25, #kgCO2/meal
+#         "Waste": 0.1 #kgCO2/kg
+#     },
+#     "United States of America":{
+#         "Transportation": 0.225, #kgCO2/km
+#         "Electricity": 1.0, #kgCO2/Kwh
+#         "Diet": 1.325, #kgCO2/meal
+#         "Waste": 0.4 #kgCO2/kg
+#     },
+#     "China": {
+#         "Transportation": 0.18,   # kgCO2/km
+#         "Electricity": 0.95,      # kgCO2/Kwh
+#         "Diet": 1.2,              # kgCO2/meal
+#         "Waste": 0.3              # kgCO2/kg
+#     },
+#     "Brazil": {
+#         "Transportation": 0.175,  # kgCO2/km
+#         "Electricity": 0.45,      # kgCO2/Kwh
+#         "Diet": 1.15,             # kgCO2/meal
+#         "Waste": 0.2              # kgCO2/kg
+#     },
+#     "Russia": {
+#         "Transportation": 0.2,    # kgCO2/km
+#         "Electricity": 0.8,       # kgCO2/Kwh
+#         "Diet": 1.3,              # kgCO2/meal
+#         "Waste": 0.35             # kgCO2/kg
+#     },
+#     "Canada": {
+#         "Transportation": 0.22,   # kgCO2/km
+#         "Electricity": 0.7,       # kgCO2/Kwh
+#         "Diet": 1.35,             # kgCO2/meal
+#         "Waste": 0.45             # kgCO2/kg
+#     },
+#     "Australia": {
+#         "Transportation": 0.19,   # kgCO2/km
+#         "Electricity": 0.9,       # kgCO2/Kwh
+#         "Diet": 1.25,             # kgCO2/meal
+#         "Waste": 0.35             # kgCO2/kg
+#     },
+#     "Japan": {
+#         "Transportation": 0.17,   # kgCO2/km
+#         "Electricity": 0.75,      # kgCO2/Kwh
+#         "Diet": 1.4,              # kgCO2/meal
+#         "Waste": 0.25             # kgCO2/kg
+#     }
+    
+        
+# }
+
+
+# st.markdown(
+#     """
+#     <style>
+#     #MainMenu {visibility: hidden;}
+#     footer {visibility: hidden;}
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+# st.markdown(
+#     r"""
+#     <style>
+#     .stDeployButton {
+#             visibility: hidden;
+#         }
+#     [data-testid="stStatusWidget"] {
+#     visibility: hidden;
+# }
+#     </style>
+#     """, unsafe_allow_html=True
+    
+    
+# )
+
+
+# query_params = get_query_params()
+# user_name = query_params.get("user_name", "")
+
+# st.title(f"Welcome, {user_name}")
+# st.title("Personal Carbon Calculator App")
+
+
+# #user inputs
+# st.subheader("🌎 Your Country")
+# country = st.selectbox("Select", ["SriLanka","India", "United States of America", "China", "Brazil", "Russia", "Canada", "Australia", "Japan"])
+
+# col1 , col2 = st.columns(2)
+
+# with col1:
+#     st.subheader("🚗 Daily Commute Distance (in KM)")
+#     distance = st.slider("Distance", 0, 100, key="distance_input")
+    
+#     st.subheader("💡 Monthly Electricity Consumption (in Kwh)")
+#     electricity = st.slider("Electricity", 0.0, 1000.0, key="electricity_input")
+    
+# with col2:
+
+#     st.subheader("🗑 Waste generated per week (in KG)")
+#     meals = st.slider("Waste", 0, 100, key="waste_input")
+    
+#     st.subheader("🍮 Number of meals per-day")
+#     waste = st.number_input("Meals", 0, 10, key="meals_input")
+
+# #Normalized
+
+# if distance > 0:
+#     distance = distance * 365 #convert daily to yearly
+    
+# if electricity > 0:
+#     electricity = electricity * 12
+    
+# if meals > 0:
+#     meals = meals * 365
+
+# if waste > 0:
+#     waste = waste * 52
+    
+# #calculate carbon emissions
+
+# transportation_emission = EMISSION_FACTORS[country]["Transportation"] * distance
+# electricity_emission = EMISSION_FACTORS[country]["Electricity"] * electricity
+# diet_emission = EMISSION_FACTORS[country]["Diet"] * meals
+# waste_emission = EMISSION_FACTORS[country]["Waste"] * waste
+
+# transportation_emission = round(transportation_emission / 1000, 2)
+# electricity_emission = round(electricity_emission / 1000, 2)
+# diet_emission = round(diet_emission / 1000, 2)
+# waste_emission = round(waste_emission / 1000, 2)
+
+
+# #convert emissions to tonnes and round off to 3 decimal points
+# total_emissions = round(transportation_emission + electricity_emission + diet_emission + waste_emission, 2)
+
+# if st.button("Calculate CO2 Emissions"):
+    
+#     st.header("Results")
+    
+#     col3, col4 = st.columns(2)
+    
+#     with col3:
+#         st.subheader("Carbon Emission by Catagories")
+#         st.info(f"🚗 Tranportation: {transportation_emission} tonnes CO2 per year")
+#         st.info(f"💡 Electricity: {electricity_emission} tonnes CO2 per year" )
+#         st.info(f"🍮 Diet: {diet_emission} tonnes CO2 per year" )
+#         st.info(f"🗑 Waste: {waste_emission} tonnes CO2 per year" )
+        
+#     with col4:
+#         st.subheader("Total Carbon Emissions")
+#         st.info(f"🌎 Your Total Carbon Footprint is: {total_emissions} tonnes CO2 per year")
+        
+#         if country == "India":
+#             st.warning("Per capita carbon dioxide (CO₂) emissions in India have soared in recent decades, climbing from 0.39 metric tons in 1970 to a high of 1.91 metric tons in 2022. This was an increase of 5.5 percent in comparison to 2021 levels. Total CO₂ emissions in India also reached a record high in 2022.")
+#         elif country == "SriLanka":
+#             st.warning("Per capita carbon dioxide (CO₂) emissions in Sri Lanka have been steadily increasing over the years.")
+#         elif country == "United States of America":
+#             st.warning("Per capita carbon dioxide (CO₂) emissions in the United States of America have remained high, contributing significantly to global emissions.")
+#         elif country == "China":
+#             st.warning("Per capita carbon dioxide (CO₂) emissions in China are among the highest in the world, primarily due to its large population and rapid industrialization.")
+#         elif country == "Brazil":
+#             st.warning("Per capita carbon dioxide (CO₂) emissions in Brazil are mainly driven by deforestation, agriculture, and energy production.")
+#         elif country == "Russia":
+#             st.warning("Per capita carbon dioxide (CO₂) emissions in Russia are influenced by its vast natural resources and heavy reliance on fossil fuels.")
+#         elif country == "Canada":
+#             st.warning("Per capita carbon dioxide (CO₂) emissions in Canada are primarily attributed to its energy-intensive industries and transportation sector.")
+#         elif country == "Australia":
+#             st.warning("Per capita carbon dioxide (CO₂) emissions in Australia are among the highest globally, mainly due to its reliance on coal for electricity generation.")
+#         elif country == "Japan":
+#             st.warning("Per capita carbon dioxide (CO₂) emissions in Japan are significant, with the country heavily reliant on imported fossil fuels for energy production.")
+                
+# st.markdown(
+#     """
+#     <style>
+#     .right-aligned {
+#         display: flex;
+#         justify-content: flex-end;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
