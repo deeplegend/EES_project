e393f28b741b9d0ccf4d1e4f585bcf34d0b54c16
khoshang-k <145487559+khoshang-k@users.noreply.github.com>
2024-04-14 01:17:08 +0530

    Update About.py
    Added Images

--- pages/About.py
+++ pages/About.py
@@ -1,17 +1,23 @@
 import streamlit as st
-
+from PIL import Image
 st.set_page_config(page_title="Carbon Footprint",page_icon=":footprints:",layout="wide")
+#storing images
+img_contact_form=Image.open("Images\Screenshot 2024-04-13 224751.png")
 
 with st.container():
     st.header("Understanding Carbon Footprint :footprints:")
     st.write("---")
 with st.container():
-    st.write("Have you ever wondered about the impact of your daily actions on the environment? Imagine every time you use electricity, drive a car, or even eat food, you're leaving behind a mark on the planet. This mark is called your carbon footprint.")
-    st.subheader("But what exactly is a carbon footprint?")
-    st.write("Well, think of it as a measure of how much carbon dioxide and other greenhouse gases you produce as a result of your activities. These gases trap heat in the atmosphere, contributing to global warming and climate change. Your carbon footprint isn't just about the big things like driving a car or using electricity. It's also about the small everyday choices you make, like the food you eat, the products you buy, and even how you travel. For example, every time you turn on a light or watch TV, you're using electricity generated from fossil fuels like coal or natural gas. Burning these fuels releases carbon dioxide into the air, adding to your carbon footprint. Similarly, when you drive a car or take a flight, the burning of fuel releases greenhouse gases into the atmosphere, contributing to your carbon footprint. Even the food you eat has a carbon footprint, depending on how it's produced and transported.")
-    st.subheader("So, why is it important to understand your carbon footprint?")
-    st.write("Well, by understanding how your actions affect the environment, you can take steps to reduce your impact and live a more sustainable lifestyle. This could mean using energy more efficiently, choosing greener modes of transportation, or consuming less meat and dairy products. By reducing your carbon footprint, you're not only helping the environment but also contributing to a healthier planet for future generations. In summary, your carbon footprint is a measure of the impact your everyday actions have on the environment. By understanding and reducing your carbon footprint, you can play a part in combating climate change and creating a more sustainable world for all.")
-    st.write("---")
+    text_column,image_column=st.columns((1,0.8))
+    with text_column:
+        st.write("Have you ever wondered about the impact of your daily actions on the environment? Imagine every time you use electricity, drive a car, or even eat food, you're leaving behind a mark on the planet. This mark is called your carbon footprint.")
+        st.subheader("But what exactly is a carbon footprint?")
+        st.write("Well, think of it as a measure of how much carbon dioxide and other greenhouse gases you produce as a result of your activities. These gases trap heat in the atmosphere, contributing to global warming and climate change. Your carbon footprint isn't just about the big things like driving a car or using electricity. It's also about the small everyday choices you make, like the food you eat, the products you buy, and even how you travel. For example, every time you turn on a light or watch TV, you're using electricity generated from fossil fuels like coal or natural gas. Burning these fuels releases carbon dioxide into the air, adding to your carbon footprint. Similarly, when you drive a car or take a flight, the burning of fuel releases greenhouse gases into the atmosphere, contributing to your carbon footprint. Even the food you eat has a carbon footprint, depending on how it's produced and transported.")
+        st.subheader("So, why is it important to understand your carbon footprint?")
+        st.write("Well, by understanding how your actions affect the environment, you can take steps to reduce your impact and live a more sustainable lifestyle. This could mean using energy more efficiently, choosing greener modes of transportation, or consuming less meat and dairy products. By reducing your carbon footprint, you're not only helping the environment but also contributing to a healthier planet for future generations. In summary, your carbon footprint is a measure of the impact your everyday actions have on the environment. By understanding and reducing your carbon footprint, you can play a part in combating climate change and creating a more sustainable world for all.")
+        st.write("---")
+    with image_column:
+        st.image(img_contact_form)
 with st.container():
     st.header("Carbon Emissions in India")
     st.subheader("1. Air Pollution: ")
