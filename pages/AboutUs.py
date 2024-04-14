2164f798cd9962c198771eb30be9eb9f52b999aa
khoshang-k <145487559+khoshang-k@users.noreply.github.com>
2024-04-14 08:59:14 +0530

    Added Github profiles

--- pages/AboutUs.py
+++ pages/AboutUs.py
@@ -16,16 +16,16 @@ with st.container():
     st.write("The core functionality of the \"Carbon Footprint Calculator\" revolves around its ability to calculate carbon emissions based on user inputs. Through a simple and intuitive user interface, users can input data such as their daily commute distance, monthly electricity consumption, meals per day, and waste generation. These inputs are then processed using predefined emission factors for different activities and regions to calculate the user's total carbon footprint.")
     st.write("One of the key features of the project is its integration with Google Sheets for data storage and management. By allowing users to submit their personal details through a form embedded in the application, the project facilitates the collection of data for analysis and reporting.")
     st.write("---")
-with st.container():
-    st.header("About Us")
-    st.write("")
-    st.write("---")
+#with st.container():
+#    st.header("About Us")
+#    st.write("")
+#    st.write("---")
 with st.container():
     st.header("**The Team**")
     Ankit_column,Deep_column,Devu_column,Khoshang_column,Kunal_column=st.columns((1,1,1,1,1))
     with Ankit_column:
         st.image(image_14,caption="Ankit Baidsen\n (IMT-014)")
-        col1,col2,col3=st.columns(3)
+        col1,col2,col3,col4,col5=st.columns(5)
         with col1:
             st.write('')
         with col2:
@@ -35,10 +35,16 @@ with st.container():
                 </a>""".format(
                 base64.b64encode(open("D:\EES_project-1\Images\LinkedIn_icon.png", "rb").read()).decode()),unsafe_allow_html=True,)
         with col3:
+            st.markdown(
+                """<a href="https://github.com/neetance">
+                <img src="data:image/png;base64,{}" width="20">
+                </a>""".format(
+                base64.b64encode(open("Images\Github_icon.png", "rb").read()).decode()),unsafe_allow_html=True,)
+        with col4:
             st.write('')
     with Deep_column:
         st.image(image_29,caption="Deep Patel\n (IMT-029)")
-        col1,col2,col3=st.columns(3)
+        col1,col2,col3,col4=st.columns(4)
         with col1:
             st.write('')
         with col2:
@@ -48,10 +54,16 @@ with st.container():
                 </a>""".format(
                 base64.b64encode(open("Images\LinkedIn_icon.png", "rb").read()).decode()),unsafe_allow_html=True,)
         with col3:
+            st.markdown(
+                """<a href="https://github.com/deeplegend">
+                <img src="data:image/png;base64,{}" width="20">
+                </a>""".format(
+                base64.b64encode(open("Images\Github_icon.png", "rb").read()).decode()),unsafe_allow_html=True,)
+        with col4:
             st.write('')
     with Devu_column:
         st.image(image_30,caption="Devu Gupta\n (IMT-030)")
-        col1,col2,col3=st.columns(3)
+        col1,col2,col3,col4=st.columns(4)
         with col1:
             st.write('')
         with col2:
@@ -61,10 +73,16 @@ with st.container():
                 </a>""".format(
                 base64.b64encode(open("Images\LinkedIn_icon.png", "rb").read()).decode()),unsafe_allow_html=True,)
         with col3:
+            st.markdown(
+                """<a href="https://github.com/codealpha07">
+                <img src="data:image/png;base64,{}" width="20">
+                </a>""".format(
+                base64.b64encode(open("Images\Github_icon.png", "rb").read()).decode()),unsafe_allow_html=True,)
+        with col4:
             st.write('')
     with Khoshang_column:
         st.image(image_42,caption="Khoshang Kashyap\n (IMT-042)")
-        col1,col2,col3=st.columns(3)
+        col1,col2,col3,col4=st.columns(4)
         with col1:
             st.write('')
         with col2:
@@ -74,10 +92,16 @@ with st.container():
                 </a>""".format(
                 base64.b64encode(open("Images\LinkedIn_icon.png", "rb").read()).decode()),unsafe_allow_html=True,)
         with col3:
+            st.markdown(
+                """<a href="https://github.com/khoshang-k">
+                <img src="data:image/png;base64,{}" width="20">
+                </a>""".format(
+                base64.b64encode(open("Images\Github_icon.png", "rb").read()).decode()),unsafe_allow_html=True,)
+        with col4:
             st.write('')
     with Kunal_column:
         st.image(image_46,caption="Kunal Agrawal\n (IMT-046)")
-        col1,col2,col3=st.columns(3)
+        col1,col2,col3,col4=st.columns(4)
         with col1:
             st.write('')
         with col2:
@@ -87,6 +111,12 @@ with st.container():
                 </a>""".format(
                 base64.b64encode(open("Images\LinkedIn_icon.png", "rb").read()).decode()),unsafe_allow_html=True,)
         with col3:
+            st.markdown(
+                """<a href="https://github.com/Kunal-8799">
+                <img src="data:image/png;base64,{}" width="20">
+                </a>""".format(
+                base64.b64encode(open("Images\Github_icon.png", "rb").read()).decode()),unsafe_allow_html=True,)
+        with col4:
             st.write('')
     st.write("---")
     
