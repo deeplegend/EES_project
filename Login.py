5ee83e0b01377eb37467a239b6af0d29e8238647
deep_patel <102026937+deeplegend@users.noreply.github.com>
2024-04-15 14:19:15 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -26,9 +26,6 @@ with st.form(key='personal_form'):
         elif data["Name"].str.contains(person_name).any():
             st.warning("Another person with same name already exists.")
             st.stop()
-        elif data["Aadhar"].str.contains(aadhar).any():
-            st.warning("Another person with same Aadhar No already exists.")
-            st.stop()
         else:
             person_data=pd.DataFrame(
                 [
